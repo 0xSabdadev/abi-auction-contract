dfdf59a7daadca58f677c9e0db02b21795e15997
0xSabdadev <jasonalhilal@gmail.com>
2024-12-16 18:59:35 +0700

    interface - main - test

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -17,4 +17,177 @@ contract Auction is IAuction, ReentrancyGuard, Ownable {
         bool ended;
         bool FundsDistributed;
     }
+
+    uint256 private _auctionIdCounter;
+    mapping (uint256 => AuctionInfo) public auctions;
+    mapping (uint256 => mapping (address => uint256)) public bids;
+
+    error AuctionNotFound();
+    error AuctionAlreadyEnded();
+    error AuctionNotEnded();
+    error BidTooLow();
+    error AuctionStillActive();
+    error FundsAlreadyDistributed();
+    error NotAuctionOwner();
+    error TransferFailed();
+
+    modifier auctionExists(uint256 auctionId) {
+        if (auctions[auctionId].owner == address(0)){
+            revert AuctionNotFound();
+        }
+        _;
+    }
+
+    modifier auctionActive(uint256 auctionId) {
+        if (auctions[auctionId].ended){
+            revert AuctionAlreadyEnded();
+        }
+        if (block.timestamp >= auctions[auctionId].endTime) {
+            revert AuctionAlreadyEnded();
+        }
+        _;
+    }
+
+    modifier auctionEnded(uint256 auctionId) {
+        if (!auctions[auctionId].ended && block.timestamp < auctions[auctionId].endTime) {
+            revert AuctionNotEnded();
+        }
+        _;
+    }
+
+    function createAuction(
+        string memory description,
+        uint256 startingPrice,
+        uint256 duration
+    ) external override returns (uint256) {
+        uint256 auctionId = _auctionIdCounter++;
+        
+        auctions[auctionId] = AuctionInfo({
+            description: description,
+            owner: msg.sender,
+            startingPrice: startingPrice,
+            highestBid: 0,
+            highestBidder: address(0),
+            endTime: block.timestamp + duration,
+            ended: false,
+            fundsDistributed: false
+        });
+        
+        emit AuctionCreated(
+            auctionId,
+            msg.sender,
+            startingPrice,
+            duration
+        );
+        
+        return auctionId;
+    }
+    
+    function placeBid(uint256 auctionId) 
+        external 
+        payable 
+        override 
+        auctionExists(auctionId)
+        auctionActive(auctionId)
+        nonReentrant 
+    {
+        AuctionInfo storage auction = auctions[auctionId];
+        
+        // Check if bid is high enough
+        if (msg.value <= auction.highestBid) {
+            revert BidTooLow();
+        }
+        
+        // Refund previous highest bidder
+        address previousBidder = auction.highestBidder;
+        uint256 previousBid = auction.highestBid;
+        
+        if (previousBidder != address(0)) {
+            bids[auctionId][previousBidder] += previousBid;
+        }
+        
+        // Update auction state
+        auction.highestBid = msg.value;
+        auction.highestBidder = msg.sender;
+        
+        emit BidPlaced(auctionId, msg.sender, msg.value);
+    }
+    
+    function endAuction(uint256 auctionId) 
+        external 
+        override 
+        auctionExists(auctionId) 
+    {
+        AuctionInfo storage auction = auctions[auctionId];
+        
+        if (block.timestamp < auction.endTime) {
+            revert AuctionStillActive();
+        }
+        
+        if (auction.ended) {
+            revert AuctionAlreadyEnded();
+        }
+        
+        auction.ended = true;
+        
+        emit AuctionEnded(
+            auctionId,
+            auction.highestBidder,
+            auction.highestBid
+        );
+    }
+    
+    function withdrawFunds(uint256 auctionId) 
+        external 
+        override 
+        auctionExists(auctionId)
+        auctionEnded(auctionId)
+        nonReentrant 
+    {
+        AuctionInfo storage auction = auctions[auctionId];
+        
+        if (msg.sender == auction.owner) {
+            if (auction.fundsDistributed) {
+                revert FundsAlreadyDistributed();
+            }
+            
+            auction.fundsDistributed = true;
+            uint256 amount = auction.highestBid;
+            
+            (bool success, ) = auction.owner.call{value: amount}("");
+            if (!success) {
+                revert TransferFailed();
+            }
+            
+            emit FundsDistributed(auctionId, auction.owner, amount);
+        } else {
+            uint256 amount = bids[auctionId][msg.sender];
+            if (amount > 0) {
+                bids[auctionId][msg.sender] = 0;
+                
+                (bool success, ) = msg.sender.call{value: amount}("");
+                if (!success) {
+                    revert TransferFailed();
+                }
+            }
+        }
+    }
+    
+    // View functions
+    function getAuction(uint256 auctionId) 
+        external 
+        view 
+        returns (AuctionInfo memory) 
+    {
+        return auctions[auctionId];
+    }
+    
+    function getBid(uint256 auctionId, address bidder) 
+        external 
+        view 
+        returns (uint256) 
+    {
+        return bids[auctionId][bidder];
+    }
+
 }
--- test/Auction.test.js
+++ test/Auction.test.js
@@ -1,126 +1,168 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
-
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
-
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
-
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
+describe("Auction", function () {
+    let Auction;
+    let auction;
+    let owner;
+    let bidder1;
+    let bidder2;
+    
+    const AUCTION_DURATION = 3600; // 1 hour
+    const STARTING_PRICE = ethers.utils.parseEther("1.0");
+    
+    beforeEach(async function () {
+        // Get signers
+        [owner, bidder1, bidder2] = await ethers.getSigners();
+        
+        // Deploy contract
+        Auction = await ethers.getContractFactory("Auction");
+        auction = await Auction.deploy();
+        await auction.deployed();
     });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
+    
+    describe("Create Auction", function () {
+        it("Should create new auction with correct parameters", async function () {
+            const tx = await auction.createAuction(
+                "Test Item",
+                STARTING_PRICE,
+                AUCTION_DURATION
+            );
+            
+            const receipt = await tx.wait();
+            const event = receipt.events?.find(e => e.event === 'AuctionCreated');
+            
+            expect(event).to.not.be.undefined;
+            expect(event.args.auctionId).to.equal(0);
+            expect(event.args.owner).to.equal(owner.address);
+            expect(event.args.startingPrice).to.equal(STARTING_PRICE);
+        });
     });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.target)).to.equal(
-        lockedAmount
-      );
+    
+    describe("Place Bid", function () {
+        let auctionId;
+        
+        beforeEach(async function () {
+            const tx = await auction.createAuction(
+                "Test Item",
+                STARTING_PRICE,
+                AUCTION_DURATION
+            );
+            const receipt = await tx.wait();
+            auctionId = receipt.events[0].args.auctionId;
+        });
+        
+        it("Should accept valid bid", async function () {
+            const bidAmount = STARTING_PRICE.mul(2);
+            
+            const tx = await auction.connect(bidder1).placeBid(auctionId, {
+                value: bidAmount
+            });
+            
+            const receipt = await tx.wait();
+            const event = receipt.events?.find(e => e.event === 'BidPlaced');
+            
+            expect(event.args.bidder).to.equal(bidder1.address);
+            expect(event.args.amount).to.equal(bidAmount);
+        });
+        
+        it("Should reject bid lower than current highest", async function () {
+            const highBid = STARTING_PRICE.mul(2);
+            const lowBid = STARTING_PRICE;
+            
+            await auction.connect(bidder1).placeBid(auctionId, {
+                value: highBid
+            });
+            
+            await expect(
+                auction.connect(bidder2).placeBid(auctionId, {
+                    value: lowBid
+                })
+            ).to.be.revertedWith("BidTooLow");
+        });
     });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
+    
+    describe("End Auction", function () {
+        let auctionId;
+        
+        beforeEach(async function () {
+            const tx = await auction.createAuction(
+                "Test Item",
+                STARTING_PRICE,
+                AUCTION_DURATION
+            );
+            const receipt = await tx.wait();
+            auctionId = receipt.events[0].args.auctionId;
+        });
+        
+        it("Should not end auction before time", async function () {
+            await expect(
+                auction.endAuction(auctionId)
+            ).to.be.revertedWith("AuctionStillActive");
+        });
+        
+        it("Should end auction after time", async function () {
+            // Increase time
+            await ethers.provider.send("evm_increaseTime", [AUCTION_DURATION + 1]);
+            await ethers.provider.send("evm_mine");
+            
+            const tx = await auction.endAuction(auctionId);
+            const receipt = await tx.wait();
+            
+            const event = receipt.events?.find(e => e.event === 'AuctionEnded');
+            expect(event).to.not.be.undefined;
+        });
     });
-  });
-
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
+    
+    describe("Withdraw Funds", function () {
+        let auctionId;
+        
+        beforeEach(async function () {
+            const tx = await auction.createAuction(
+                "Test Item",
+                STARTING_PRICE,
+                AUCTION_DURATION
+            );
+            const receipt = await tx.wait();
+            auctionId = receipt.events[0].args.auctionId;
+            
+            // Place bids
+            await auction.connect(bidder1).placeBid(auctionId, {
+                value: STARTING_PRICE.mul(2)
+            });
+            
+            await auction.connect(bidder2).placeBid(auctionId, {
+                value: STARTING_PRICE.mul(3)
+            });
+            
+            // End auction
+            await ethers.provider.send("evm_increaseTime", [AUCTION_DURATION + 1]);
+            await ethers.provider.send("evm_mine");
+            await auction.endAuction(auctionId);
+        });
+        
+        it("Should allow winner to withdraw funds", async function () {
+            const initialBalance = await owner.getBalance();
+            
+            await auction.withdrawFunds(auctionId);
+            
+            const finalBalance = await owner.getBalance();
+            expect(finalBalance.sub(initialBalance)).to.be.closeTo(
+                STARTING_PRICE.mul(3),
+                ethers.utils.parseEther("0.01") // Allow for gas costs
+            );
+        });
+        
+        it("Should allow losing bidder to withdraw", async function () {
+            const initialBalance = await bidder1.getBalance();
+            
+            await auction.connect(bidder1).withdrawFunds(auctionId);
+            
+            const finalBalance = await bidder1.getBalance();
+            expect(finalBalance.sub(initialBalance)).to.be.closeTo(
+                STARTING_PRICE.mul(2),
+                ethers.utils.parseEther("0.01") // Allow for gas costs
+            );
+        });
     });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
-  });
 });
